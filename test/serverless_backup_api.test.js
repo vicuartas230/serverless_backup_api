7510a67d2c8a200107e4cfef302591a729260455
vicuartas230 <vicuartas230@gmail.com>
2024-10-26 12:40:35 -0500

    Initial commit

---
+++ test/serverless_backup_api.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ServerlessBackupApi = require('../lib/serverless_backup_api-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless_backup_api-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ServerlessBackupApi.ServerlessBackupApiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
